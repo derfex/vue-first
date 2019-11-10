01556b488948379a982b95b47e44e6e7b2a367f5
derfex <dm1tr0@mail.ru>
2019-11-10 20:34:27 +0300

    Vuex (before). ClickCounter. Create the component with a simple state

---
+++ src/components/Works/ClickCounter.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <p>{{ count }}</p>
+        <p>
+            <button @click="increment">+</button>
+            <button @click="decrement">–</button>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ClickCounter',
+
+        // State.
+        data() {
+            return {
+                count: 0,
+            }
+        },
+
+        // Actions.
+        methods: {
+            increment() {
+                this.count++
+            },
+            decrement() {
+                this.count--
+            },
+        },
+    }
+</script>
