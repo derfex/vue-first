b8f1869f6c1c00d9ad68a0cc11c7b6efe0ddee8e
derfex <derfex@yandex.ru>
2024-07-18 04:10:05 +0300

    #38. Refactor (counter.ts): Specify the types

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,12 +1,18 @@
 import { defineStore } from 'pinia'
-import { computed, ref } from 'vue'
+import { computed, ref, type ComputedRef, type Ref } from 'vue'
 
-export const useCounterStore = defineStore('counter', () => {
+export const useCounterStore = defineStore('counter', (): CounterStore => {
   const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
+  const doubleCount = computed((): number => count.value * 2)
+  function increment(): void {
     count.value++
   }
 
   return { count, doubleCount, increment }
 })
+
+interface CounterStore {
+  readonly count: Ref<number>
+  readonly doubleCount: ComputedRef<number>
+  readonly increment: () => void
+}
